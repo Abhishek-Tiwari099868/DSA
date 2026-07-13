d65878fbb035015ec2da7839f4ba79781e4e6a2d
Abhishek Tiwari <at0263690@gmail.com>
2026-07-13 20:05:01 +0530

    Added solution - LeetHub

---
+++ 703457-min-and-max-in-array-GFG/703457-min-and-max-in-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+      int min = arr[0];
+      int max =arr[0];
+      
+      for(int num :arr){
+          if (num < min){
+              min = num;
+          }
+          
+          if(num > max){
+              max = num;
+          }
+      }
+      
+      ArrayList<Integer> result = new ArrayList<>();
+      result.add(min);
+      result.add(max);
+      
+      return result;
+      
+      
+      
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
