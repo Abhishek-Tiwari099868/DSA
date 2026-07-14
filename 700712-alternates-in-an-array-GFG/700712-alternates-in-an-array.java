4cb36d478f85bbd15364aeab8bbe3c73165bd7c5
Abhishek Tiwari <at0263690@gmail.com>
2026-07-14 20:28:30 +0530

    Added solution - Time: O(n) ,Space: O(n)

---
+++ 700712-alternates-in-an-array-GFG/700712-alternates-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                result.add(arr[i]);
+            }
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
