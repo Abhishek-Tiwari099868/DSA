b17feac94494c138a21d89769df014ba22f1945e
Abhishek Tiwari <at0263690@gmail.com>
2026-07-20 09:27:52 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 703289-segregate-0s-and-1s-GFG/703289-segregate-0s-and-1s.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    void segregate0and1(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left<right) {
+
+            if (arr[left] == 0) {
+                left++;
+            }
+
+            else if (arr[right] == 1) {
+               right--;
+            }
+
+            else {
+
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
