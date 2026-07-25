f4324d3c88d67c5ea469bcab1c45c1be42e660d9
Abhishek Tiwari <at0263690@gmail.com>
2026-07-25 11:09:20 +0530

    Update 0075-sort-colors.java

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -30,6 +30,41 @@ class Solution {
     }
 }
 
+//2approach optimized
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int start = 0;
+        int mid = 0;
+        int end = nums.length - 1;
+
+        while (mid <= end) {
+
+            if (nums[mid] == 0) {
+
+                int temp = nums[start];
+                nums[start] = nums[mid];
+                nums[mid] = temp;
+
+                start++;
+                mid++;
+
+            } else if (nums[mid] == 1) {
+
+                mid++;
+
+            } else { 
+
+                int temp = nums[mid];
+                nums[mid] = nums[end];
+                nums[end] = temp;
+
+                end--;
+            }
+        }
+    }
+}
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
